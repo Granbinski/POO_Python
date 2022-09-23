2524c438c529d6689371021e5a4bcb1bfc23f7b6
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2022-09-23 15:54:30 -0400

    update

--- new.py
+++ new.py
@@ -1,12 +1,19 @@
+from conta import Conta
+
+
 def cria_conta(numero, titular, saldo, limite):
-    conta = {"numero": numero, "titular": titular, "saldo":saldo, "limite":limite}
+    conta = {"numero": numero, "titular": titular,
+             "saldo": saldo, "limite": limite}
     return conta
+
 
 def deposita(conta, valor):
     conta["saldo"] += valor
 
+
 def saca(conta, valor):
     conta["saldo"] -= valor
 
-def extrato(conta, valor):
+
+def extrato(conta):
     print("Saldo é {}".format(conta["saldo"]))
