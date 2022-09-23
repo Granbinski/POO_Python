e8d17df6071aded85d6a5c28a2acab0bd92a26ae
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2022-09-23 19:41:30 -0400

    update

--- conta.py
+++ conta.py
@@ -2,4 +2,18 @@
 
 class Conta:
 
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        print("Initializing")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
